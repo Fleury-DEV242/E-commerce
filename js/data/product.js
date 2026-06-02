0324290f2930a3f274f8eb29e96c0c09cda8d366
fleury <fleurytaty242@gmail.com>
2026-06-03 00:00:58 +0100

    feat: e-commerce v1.0

---
+++ js/data/product.js
@@ -0,0 +1,63 @@
+const mesProduits = [
+  {
+    id: 1,
+    nom: "Nike Air Force 1 Blanche",
+    description:
+      "Ceci est une Nike Air Force, une paire à la fois chic et à la mode",
+    prix: 11000,
+    image: "../../assets/image/air_force_one.jpg",
+    marque: "NIKE",
+    stock: 10,
+  },
+  {
+    id: 2,
+    nom: "New Balance 740",
+    description:
+      "Ceci est une New Balance 740, une paire à la fois chic et à la mode",
+    prix: 10000,
+    image: "../../assets/image/new_balance_740.jpg",
+    marque: "NEW BALANCE",
+    stock: 10,
+  },
+  {
+    id: 3,
+    nom: "Nike Air Force 1 Black",
+    description:
+      "Ceci est une Nike Air Force 1, une paire à la fois chic et à la mode",
+    prix: 11000,
+    image: "../../assets/image/nike_air_force_black.jpg",
+    marque: "NIKE",
+    stock: 10,
+  },
+  {
+    id: 4,
+    nom: "Nike Nocta Rouge",
+    description:
+      "Ceci est une Nike Nocta, une paire à la fois chic et à la mode",
+    prix: 15000,
+    image: "../../assets/image/nike_nocta_rouge.jpg",
+    marque: "NIKE",
+    stock: 10,
+  },
+  {
+    id: 5,
+    nom: "Nike TN Blanche",
+    description: "Ceci est une Nike TN, une paire à la fois chic et à la mode",
+    prix: 12000,
+    image: "../../assets/image/nike_tn.jpg",
+    marque: "NIKE",
+    stock: 10,
+  },
+  {
+    id: 6,
+    nom: "Nike Air Force 1 Blanche",
+    description:
+      "Ceci est une Nike Air Force, une paire à la fois chic et à la mode",
+    prix: 11000,
+    image: "../../assets/image/air_force_one.jpg",
+    marque: "NIKE",
+    stock: 10,
+  },
+];
+
+export { mesProduits };
